cb7f7a4c22ec87c51efe549f084a5eb4095f36e6
Maks747 <valol93@mail.ru>
2025-06-21 18:27:33 +0300

    Update

--- demoqa_tests/model/pages/registration_page.py
+++ demoqa_tests/model/pages/registration_page.py
@@ -21,8 +21,7 @@ class RegistrationPage:
         ).click()
         for sub in user.subjects.split(", "):
             browser.element('#subjectsInput').type(sub).press_enter()
-        for hob in user.hobbies.split(", "):
-            browser.all('.custom-checkbox').element_by(have.exact_text(user.hob)).click()
+        browser.all('.custom-checkbox').element_by(have.exact_text(user.hobbies)).click()
 
         browser.element('#uploadPicture').set_value(
             os.path.abspath(f'resources/{user.picture}')
