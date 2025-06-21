fbd137132ab63cb343427eae8382f668132b291a
Maks747 <valol93@mail.ru>
2025-06-21 19:13:35 +0300

    Update

--- demoqa_tests/model/pages/registration_page.py
+++ demoqa_tests/model/pages/registration_page.py
@@ -10,6 +10,9 @@ class RegistrationPage:
     def open(self):
             browser.open('https://demoqa.com/automation-practice-form')
 
+            browser.driver.execute_script("$('#fixedban').remove()")
+            browser.driver.execute_script("$('footer').remove()")
+
     def fill_first_name(self, value):
         browser.element('#firstName').type(value)
 
