271c9233323cd2ad6d2cbf40daf93538d967ac1a
Maks747 <valol93@mail.ru>
2025-06-21 18:15:56 +0300

    Update

--- demoqa_tests/model/pages/registration_page.py
+++ demoqa_tests/model/pages/registration_page.py
@@ -21,7 +21,7 @@ class RegistrationPage:
         ).click()
         for sub in user.subjects.split(", "):
             browser.element('#subjectsInput').type(sub).press_enter()
-        browser.all('#hobbiesWrapper').element_by(have.exact_text(user.hobbies)).click()
+        browser.element('#hobbiesWrapper label').element_by(have.exact_text(user.hobbies)).click()
 
         browser.element('#uploadPicture').set_value(
             os.path.abspath(f'resources/{user.picture}')
